d7dc8f0bb92d2c89fad92d1ae7356aa49ccedfea
Rukiye <76444299+rkymtc@users.noreply.github.com>
2021-11-26 21:25:50 +0300

    algorithm exercises

---
+++ exercises/algorithm_01.js
@@ -0,0 +1,20 @@
+//  [Problem]
+//  üç sayıdan en küçüğünü veren bir program yazın.
+
+// --> Girdi Örneği 1
+// 6 5 11
+
+// --> Çıktı Örneği 1
+// 5
+
+// Bu basit bir if koşulu.
+// Minimum değeri bulmak için bir değişken kullanmamız yeterlidir.
+
+function solution(a, b, c) {
+  let answer;
+  a < b ? (answer = a) : (answer = b);
+  c < answer ? (answer = c) : answer;
+  return answer;
+}
+
+console.log(solution(6, 5, 11));
